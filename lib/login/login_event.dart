1aadc85392d0b2d8800b960029bb56792c510efb
Stefan <32258096+StefanYYC@users.noreply.github.com>
2020-07-09 10:28:33 +0200

    ajout du bloc login complété

---
+++ lib/login/login_event.dart
@@ -0,0 +1,55 @@
+import 'package:meta/meta.dart';
+import 'package:equatable/equatable.dart';
+
+abstract class LoginEvent extends Equatable {
+  const LoginEvent();
+
+  @override
+  List<Object> get props => [];
+}
+// When user is modifying the input of the email field
+class LoginEmailChanged extends LoginEvent {
+  final String email;
+
+  const LoginEmailChanged({@required this.email});
+
+  @override
+  List<Object> get props => [email];
+
+  @override
+  String toString() => 'LoginEmailChanged { email :$email }';
+}
+// Same for the password field
+class LoginPasswordChanged extends LoginEvent {
+  final String password;
+
+  const LoginPasswordChanged({@required this.password});
+
+  @override
+  List<Object> get props => [password];
+
+  @override
+  String toString() => 'LoginPasswordChanged { password: $password }';
+}
+
+// When the user tap on the Google Icon to Login
+class LoginWithGooglePressed extends LoginEvent {}
+
+// When the user tap on the Login button
+class LoginWithCredentialsPressed extends LoginEvent {
+  final String email;
+  final String password;
+
+  const LoginWithCredentialsPressed({
+    @required this.email,
+    @required this.password,
+  });
+
+  @override
+  List<Object> get props => [email, password];
+
+  @override
+  String toString() {
+    return 'LoginWithCredentialsPressed { email: $email, password: $password }';
+  }
+}
